e6bf0348e39609db58ab1d925cdf2ed801a69822
Dmitry Matyushkin <dmatushkin@gmail.com>
2017-05-21 11:13:34 +0400

    Initial Commit

---
+++ ShoppingManiac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingManiac
+//
+//  Created by Dmitry Matyushkin on 21/05/2017.
+//  Copyright © 2017 Dmitry Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
