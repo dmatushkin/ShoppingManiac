8fdc2f07b402d3756a3af46292d4eeac303da4af
Dmitry Matyushkin <dmatushkin@gmail.com>
2020-04-05 19:38:52 +0400

    Unused library removed, all others updated to latest versions

--- ShoppingManiac/UI/ViewControllers/ShoppingManiacViewController/ShoppingManiacViewController.swift
+++ ShoppingManiac/UI/ViewControllers/ShoppingManiacViewController/ShoppingManiacViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import MessageUI
-import SWCompression
 
 class ShoppingManiacViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
@@ -20,27 +19,7 @@ class ShoppingManiacViewController: UIViewController, MFMailComposeViewControlle
         super.viewWillDisappear(animated)
         self.view.endEditing(true)
     }
-    
-    /*override var canBecomeFirstResponder: Bool {
-        return true
-    }
-    
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if motion == UIEventSubtype.motionShake {
-            let cacheUrls = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
-            for url in cacheUrls {
-                let logUrl = url.appendingPathComponent("swiftybeaver.log")
-                if let logData = try? Data(contentsOf: logUrl), MFMailComposeViewController.canSendMail() {
-                    let composer = MFMailComposeViewController()
-                    composer.mailComposeDelegate = self
-                    composer.setSubject("Issue report")
-                    composer.addAttachmentData(BZip2.compress(data: logData), mimeType: "application/x-bzip2", fileName: "log.txt.bz2")
-                    self.present(composer, animated: true, completion: nil)
-                }
-            }
-        }
-    }*/
-    
+        
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
     }
